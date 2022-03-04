fa262bcbae1198b621738904aa4efb5fc87f2d66
Benn King <bennkingy@gmail.com>
2022-03-04 19:30:29 +0000

    Better

--- fundamentals/src/components/Task.vue
+++ fundamentals/src/components/Task.vue
@@ -1,10 +1,9 @@
 <template>
-    <div 
-        @dblclick="$emit('toggle-reminder', task.id)"
-        :class="[task.reminder ? 'reminder' : '', 'task']">
+    <div @dblclick="$emit('toggle-reminder', task.id)"
+    :class="[task.reminder ? 'reminder' : '', 'task']">
         <h3>
             {{ task.text }}
-            <i @click="onDelete(task.id)" class="fas fa-times"></i>
+            <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
         </h3>
         <p>{{ task.day }}</p>
     </div>
@@ -15,12 +14,6 @@
         name: 'Task',
         props: {
             task: Object
-        },
-        methods: {
-            onDelete(id) {
-                console.log(id)
-                this.$emit('delete-task', id)
-            }
         }
     }
 </script>

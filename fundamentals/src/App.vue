d778d63f2443b2b3d3357f766860a9dc47d1223a
Benn King <bennkingy@gmail.com>
2022-03-05 11:35:39 +0000

    Added routing

--- fundamentals/src/App.vue
+++ fundamentals/src/App.vue
@@ -6,25 +6,20 @@
         @add-task="addTask"
       />
     </div>
-    <Tasks
-      @delete-task="deleteTask"
-      @toggle-reminder="toggleReminder"
-      :tasks="tasks"
-    />
+    <router-view :showAddTask="showAddTask"></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
   import Header from './components/Header'
-  import Tasks from './components/Tasks'
-  import AddTask from './components/AddTask'
+  import Footer from './components/Footer'
 
   export default {
     name: 'App',
     components: {
       Header,
-      Tasks,
-      AddTask
+      Footer
     },
     data() {
       return {
@@ -34,45 +29,8 @@
     },
     methods: {
       toggleAddTask() {
-        console.log('hiii')
         this.showAddTask = !this.showAddTask
-      },
-      async addTask(task) {
-        const res = await fetch('api/tasks', {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json',
-          },
-          body: JSON.stringify(task)
-        })
-        const data = await res.json()
-        this.tasks = [...this.tasks, task]
-      },
-      async deleteTask(id) {
-        if(confirm('Are you sure?')) {
-
-          const res = await fetch(`api/tasks/${id}`, {
-            method: 'DELETE'
-          })
-          res.status === 200 ? ( this.tasks = this.tasks.filter((task) => task.id !== id) ) : alert('Error')
-        }
-      },
-      toggleReminder(id) {
-        this.tasks = this.tasks.map((task) => task.id == id ? {...task, reminder: !task.reminder} : task)
-      },
-      async fetchTasks() {
-        const res = await fetch('api/tasks');
-        const data = await res.json();
-        return data
-      },
-      async fetchTask(id) {
-        const res = await fetch(`api/tasks/${id}`);
-        const data = await res.json();
-        return data
       }
-    },
-    async created() {
-      this.tasks = await this.fetchTasks();
     }
   }
 </script>
